c7971e7d7b990f1f2de0eb71bc79bf4a53d0997c
Julian Rivera <julianrivor@gmail.com>
2014-11-28 12:59:39 -0500

    Cambie el titulo por empezamos con git

--- src/main/java/emp/imasd/project/App.java
+++ src/main/java/emp/imasd/project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Adios  a todos" );
+        System.out.println( "Empezamos con git" );
     }
 }
